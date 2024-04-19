24931165142f0a8d068a57cc441ca9c96045473a
Safouene Turki <tsafouen@gmail.com>
2024-04-19 16:24:51 +0100

    feat : cluster card hover effects and tooltips

--- src/pages/clusters-list/components/ClusterCard.tsx
+++ src/pages/clusters-list/components/ClusterCard.tsx
@@ -3,6 +3,12 @@ import { Badge } from "@/components/ui/badge";
 
 import { ReadyFlag } from "@/pages/clusters-list/components/flags/ReadyFlag";
 import { FailedFlag } from "@/pages/clusters-list/components/flags/FailedFlag";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface ClusterCardProps {
   name: string;
@@ -24,28 +30,37 @@ export const ClusterCard = ({
 }: ClusterCardProps) => {
   return (
     <>
-      <Card>
-        <div className=" flex items-center space-x-4 rounded-md  p-4">
-          {status ? <ReadyFlag /> : <FailedFlag />}
-          <div className="flex-1 space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {name}
-              <span className={"text-muted-foreground "}>{version}</span>
-            </p>
-            <p className="text-sm text-muted-foreground">
-              Namespace: <span className="text-main-500">{namespace}</span>
-            </p>
-          </div>
-          {labels?.length > 0 && (
-            <div className="flex items-center space-x-2">
-              {labels?.map((label) => (
-                <Badge key={label.designation} variant="secondary">
-                  {label.designation}
-                </Badge>
-              ))}
+      <Card className={"hover:bg-gray-100 hover:cursor-pointer"}>
+        <TooltipProvider>
+          <div className=" flex items-center space-x-4 rounded-md  p-4">
+            {status ? <ReadyFlag /> : <FailedFlag />}
+            <div className="flex-1 space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {name}
+                <span className={"text-muted-foreground mx-1"}>
+                  <Tooltip>
+                    <TooltipTrigger>{version}</TooltipTrigger>
+                    <TooltipContent>
+                      <p>Kubernetes version : {version}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </span>
+              </p>
+              <p className="text-sm text-muted-foreground">
+                Namespace: <span className="text-main-500">{namespace}</span>
+              </p>
             </div>
-          )}
-        </div>
+            {labels?.length > 0 && (
+              <div className="flex items-center space-x-2">
+                {labels?.map((label) => (
+                  <Badge key={label.designation} variant="secondary">
+                    {label.designation}
+                  </Badge>
+                ))}
+              </div>
+            )}
+          </div>
+        </TooltipProvider>
       </Card>
     </>
   );
--- src/pages/clusters-list/components/flags/FailedFlag.tsx
+++ src/pages/clusters-list/components/flags/FailedFlag.tsx
@@ -3,16 +3,36 @@ import {
   AvatarRectangle,
   AvatarRectFallback,
 } from "@/components/ui/avatar-rectangle";
-import { FlagOff } from "lucide-react";
+import {
+  AlertCircle,
+  AlertCircleIcon,
+  FlagOff,
+  MessageSquareWarningIcon,
+} from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export const FailedFlag = () => {
   return (
     <>
-      <AvatarRectangle className="h-9 w-9">
-        <AvatarRectFallback className={"bg-red-600 text-white"}>
-          <Icons.k8s className={"h-6 w-6"} />
-        </AvatarRectFallback>
-      </AvatarRectangle>
+      <Tooltip>
+        <TooltipTrigger>
+          <AvatarRectangle className="h-9 w-9">
+            <AvatarRectFallback className={"bg-red-600 text-white"}>
+              <Icons.k8s className={"h-6 w-6"} />
+            </AvatarRectFallback>
+          </AvatarRectangle>
+        </TooltipTrigger>
+        <TooltipContent>
+          <div className={"inline-flex items-baseline"}>
+            <AlertCircleIcon />
+            <p>Unhealthy</p>
+          </div>
+        </TooltipContent>
+      </Tooltip>
     </>
   );
 };
